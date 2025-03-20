7e76431de8c881dff328935d951c88a6329c1fd9
Adinan <adinanmatosjunior021@gmail.com>
2025-03-20 16:37:42 -0400

    commit

--- atividades/exercicio5.py
+++ atividades/exercicio5.py
@@ -1,7 +1,5 @@
-# Entrada do usuário
 valor = int(input("Digite o valor em centavos: "))
 
-# Definição dos valores das moedas
 moeda1 = 100
 moeda2 = 50
 moeda3 = 25
@@ -28,17 +26,16 @@ valor = valor - (qtd5 * moeda5)
 qtd6 = valor // moeda6
 valor = valor - (qtd6 * moeda6)
 
-# Exibição do resultado
 print("Menor quantidade de moedas necessárias:")
 if qtd1 > 0:
-    print(f"{qtd1} moeda(s) de 1 real")
+    print(qtd1, "moeda(s) de 1 real")
 if qtd2 > 0:
-    print(f"{qtd2} moeda(s) de 50 centavos")
+    print(qtd2, "moeda(s) de 50 centavos")
 if qtd3 > 0:
-    print(f"{qtd3} moeda(s) de 25 centavos")
+    print(qtd3, "moeda(s) de 25 centavos")
 if qtd4 > 0:
-    print(f"{qtd4} moeda(s) de 10 centavos")
+    print(qtd4, "moeda(s) de 10 centavos")
 if qtd5 > 0:
-    print(f"{qtd5} moeda(s) de 5 centavos")
+    print(qtd5, "moeda(s) de 5 centavos")
 if qtd6 > 0:
-    print(f"{qtd6} moeda(s) de 1 centavo")
+    print(qtd6, "moeda(s) de 1 centavo")
