50d5f80d1015b6abe7b3d67547d0cbaa0e92bdd8
Adinan <adinanmatosjunior021@gmail.com>
2025-03-20 01:10:18 -0400

    commit

---
+++ atividades/exercicio7.py
@@ -0,0 +1,10 @@
+#Faça um algoritmo que receba três notas, calcule e mostre a média
+#aritmética.
+
+nota1 = float(input("Digite a nota de sua primeira atividade: "))
+nota2 = float(input("Digite a nota de sua segunda atividade: "))
+nota3 = float(input("Digite a nota de sua terceira atividade: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print("Sua média é:", media)
