1874e174490dd2a5ee4ce53017fcbc9d2366ad68
Adinan <adinanmatosjunior021@gmail.com>
2025-03-20 16:33:56 -0400

    commit

--- atividades/exercicio10.py
+++ atividades/exercicio10.py
@@ -2,3 +2,9 @@
 #percentual de aumento, calcule e mostre o valor do aumento e o novo
 #salário.
 
+salario = float(input("Digite seu salario: "))
+aumento = float(input("Digite o percentual de aumento: "))
+
+novo_salario = salario + (salario * aumento)
+
+print("Seu novo salario é:", novo_salario)
