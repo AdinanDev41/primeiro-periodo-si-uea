e0fcd73a37f0d0773828b08b288a0d8ee9b151a6
Adinan <adinanmatosjunior021@gmail.com>
2025-03-20 01:06:27 -0400

    commit

---
+++ atividades/exercicio6.py
@@ -0,0 +1,11 @@
+#Faça um algoritmo que receba quatro números inteiros, calcule e
+#mostre a soma desses números.
+
+num1 = int(input("Digite o numero: "))
+num2 = int(input("Digite o numero: "))
+num3 = int(input("Digite o numero: "))
+num4 = int(input("Digite o numero: "))
+
+soma = (num1 + num2 + num3 + num4)
+
+print("O seu resultado é:", soma)
