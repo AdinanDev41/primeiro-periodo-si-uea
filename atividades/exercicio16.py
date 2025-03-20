c00030f870096fe0ec8e32f865031cfbae5af1f6
Adinan <adinanmatosjunior021@gmail.com>
2025-03-20 17:16:07 -0400

    commit

---
+++ atividades/exercicio16.py
@@ -0,0 +1,8 @@
+#Faça um algoritmo que receba uma temperatura em Celsius, calcule e
+#mostre essa temperatura em Fahrenheit. Sabe-se que: F = 180 * (c + 32)/100
+
+c = float(input("Digite o valor da temperatura em Celsius: "))
+
+f = 180 * (c + 32)/100
+
+print("A temperatura em Fahrenheit é:", f)
