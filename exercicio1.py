ba1138e3a77d15b41fa3718819df1ed64afc6b1a
Adinan <adinanmatosjunior021@gmail.com>
2025-03-20 00:20:02 -0400

    commit

---
+++ exercicio1.py
@@ -0,0 +1,11 @@
+# Faça um programa que leia o nome, a idade, a altura, o peso e
+# a nacionalidade do usuário e escreva essas informações na
+# forma de um parágrafo de apresentação
+
+nome = input("Digite seu nome:")
+idade = int(input("Digite sua idade:"))
+altura = float(input("Digite sua altura:"))
+peso = float(input("Digite seu peso:"))
+nacionalidade = input("Digite sua nacionalidade:")
+
+print("Meu nome é", nome, ", tenho", idade, "anos", ", minha altura é", altura, "m", ", meu peso é", peso,"kg", ", minha nacionalidade é", nacionalidade)
